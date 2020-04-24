532c7f4c9cdb5bef002f9e769357d55382d87580
Tristan K <tristanmkernan@gmail.com>
2020-04-24 19:07:37 -0400

    initial commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Product;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        return $request->user()->products()->get()->toJson();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|min:2|max:128',
+        ]);
+
+        return response('OK');
+
+        $product = new Product([
+            'name' => $data['name'],
+            'stocked' => $data['stocked'],
+            'user_id' => $request->user()->id
+        ]);
+
+        $product->save();
+
+        return $product;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request, $id)
+    {
+        return $request->user()->products()->find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = $request->validate([
+            'name' => 'required|min:2|max:128',
+            'stocked' => 'required|boolean'
+        ]);
+
+        $product = $request->user()->products()->find($id);
+
+        $product->name = $data['name'];
+        $product->stocked = $data['stocked'];
+
+        $product->save();
+
+        return $product;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request, $id)
+    {
+        $product = $request->user()->products()->find($id);
+
+        $product->destroy();
+
+        return response()->json(['status' => 'ok']);
+    }
+}
